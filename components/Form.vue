582e6861363cef9940bfad9a2ff85dc0df4b92c9
nrm176 <>
2021-03-25 21:21:12 +0900

    use new form pattern

--- components/Form.vue
+++ components/Form.vue
@@ -1,42 +1,95 @@
 <template>
-  <div class="bg-white py-16 px-4 sm:px-6 lg:col-span-3 lg:py-24 lg:px-8 xl:pl-12">
-    <div class="max-w-lg mx-auto lg:max-w-none">
-      <form method="POST" class="grid grid-cols-1 gap-y-6"
-            name="simpleContactForm" data-netlify="true" data-netlify-recaptcha="true"
-            id="simple-contact-form"
-            netlify-honeypot="bot-field" action="/thankyou"
-      >
-        <input type="hidden" name="form-name" value="simpleContactForm" />
-        <div>
-          <label for="full_name" class="sr-only">Full name</label>
-          <input type="text" name="full_name" id="full_name" autocomplete="name" class="block w-full shadow-sm py-3 px-4 placeholder-gray-500 focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md" placeholder="Full name">
-        </div>
-        <div>
-          <label for="email" class="sr-only">Email</label>
-          <input id="email" name="email" type="email" autocomplete="email" class="block w-full shadow-sm py-3 px-4 placeholder-gray-500 focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md" placeholder="Email">
-        </div>
-        <div>
-          <label for="phone" class="sr-only">Phone</label>
-          <input type="text" name="phone" id="phone" autocomplete="tel" class="block w-full shadow-sm py-3 px-4 placeholder-gray-500 focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md" placeholder="Phone">
-        </div>
-        <div>
-          <label for="message" class="sr-only">Message</label>
-          <textarea id="message" name="message" rows="4" class="block w-full shadow-sm py-3 px-4 placeholder-gray-500 focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md" placeholder="Message"></textarea>
-        </div>
-        <div>
-          <button type="submit" class="inline-flex justify-center py-3 px-6 border border-transparent shadow-sm text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-            Submit
-          </button>
-        </div>
-      </form>
+  <form
+      class="w-full max-w-lg"
+      name="simpleContactFormVersion2"
+      method="POST"
+      data-netlify="true"
+      data-netlify-recaptcha="true"
+      id="simple-contact-form-version-2"
+  >
+    <!-- First and last name -->
+    <div class="flex flex-wrap -mx-3 mb-6">
+      <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+        <label
+            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            for="grid-first-name"
+        >
+          First Name
+        </label>
+        <input
+            name="first_name"
+            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+            id="grid-first-name"
+            type="text"
+            placeholder="Jane"
+        />
+        <p class="text-red-500 text-xs italic">Please fill out this field.</p>
+      </div>
+      <div class="w-full md:w-1/2 px-3">
+        <label
+            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            for="grid-last-name"
+        >
+          Last Name
+        </label>
+        <input
+            name="last_name"
+            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            id="grid-last-name"
+            type="text"
+            placeholder="Doe"
+        />
+      </div>
     </div>
-  </div>
+    <!-- Email -->
+    <div class="flex flex-wrap -mx-3 mb-6">
+      <div class="w-full px-3">
+        <label
+            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            for="grid-email"
+        >
+          E-mail
+        </label>
+        <input
+            name="user_email"
+            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            id="grid-email"
+            type="email"
+        />
+      </div>
+    </div>
+    <!-- Text Message -->
+    <div class="flex flex-wrap -mx-3 mb-6">
+      <div class="w-full px-3">
+        <label
+            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            for="grid-message"
+        >
+          Message
+        </label>
+        <textarea
+            name="inquiry_text_area"
+            class="no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none"
+            id="grid-message"
+        ></textarea>
+        <p class="text-gray-600 text-xs italic">
+          Re-size can be disabled by set by resize-none / resize-y / resize-x /
+          resize
+        </p>
+      </div>
+    </div>
+    <!-- Submit Button -->
+    <div data-netlify-recaptcha="true"></div>
+    <div class="md:flex md:items-center">
+      <div class="md:w-1/3">
+        <button
+            class="shadow bg-teal-400 hover:bg-teal-400 focus:shadow-outline focus:outline-none font-bold py-2 px-4 rounded"
+            type="submit"
+        >
+          Send Form version 2
+        </button>
+      </div>
+      <div class="md:w-2/3"></div>
+    </div>
+  </form>
 </template>
-
-<style>
-@keyframes appear {
-  0% {
-    opacity: 0;
-  }
-}
-</style>
