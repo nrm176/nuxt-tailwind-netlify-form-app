bbe6997b7fc28550bc80c90c79cbf6b57d95e7dc
nrm176 <>
2021-03-25 21:28:30 +0900

    change label name

--- components/Form.vue
+++ components/Form.vue
@@ -9,35 +9,34 @@
   >
     <!-- First and last name -->
     <div class="flex flex-wrap -mx-3 mb-6">
-      <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-        <label
-            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-            for="grid-first-name"
-        >
-          First Name
-        </label>
-        <input
-            name="first_name"
-            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-            id="grid-first-name"
-            type="text"
-            placeholder="Jane"
-        />
-        <p class="text-red-500 text-xs italic">Please fill out this field.</p>
-      </div>
       <div class="w-full md:w-1/2 px-3">
         <label
             class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
             for="grid-last-name"
         >
-          Last Name
+          姓
         </label>
         <input
             name="last_name"
             class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
             id="grid-last-name"
             type="text"
-            placeholder="Doe"
+            placeholder="徳川"
+        />
+      </div>
+      <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+        <label
+            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            for="grid-first-name"
+        >
+          名
+        </label>
+        <input
+            name="first_name"
+            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+            id="grid-first-name"
+            type="text"
+            placeholder="家康"
         />
       </div>
     </div>
@@ -72,10 +71,6 @@
             class="no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none"
             id="grid-message"
         ></textarea>
-        <p class="text-gray-600 text-xs italic">
-          Re-size can be disabled by set by resize-none / resize-y / resize-x /
-          resize
-        </p>
       </div>
     </div>
     <!-- Submit Button -->
@@ -86,7 +81,7 @@
             class="shadow bg-teal-400 hover:bg-teal-400 focus:shadow-outline focus:outline-none font-bold py-2 px-4 rounded"
             type="submit"
         >
-          Send Form version 2
+          送信
         </button>
       </div>
       <div class="md:w-2/3"></div>
