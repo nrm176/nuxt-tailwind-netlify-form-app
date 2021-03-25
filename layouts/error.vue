6f75561b12cea9c12c7bedb39f87460d948c1b31
nrm176 <>
2021-03-25 21:50:29 +0900

    add error page

---
+++ layouts/error.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <h1 v-if="error.statusCode === 404">Page not found</h1>
+    <h1 v-else>An error occurred</h1>
+    <div>
+      <NuxtLink to="/">Home page</NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'error',
+  props: ['error'],
+  layout: 'default' // you can set a custom layout for the error page
+}
+</script>
--- pages/thankyou.vue
+++ pages/thankyou.vue
@@ -3,3 +3,9 @@
     <div>お問い合わせありがとうございます。</div>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'thankyou'
+}
+</script>
