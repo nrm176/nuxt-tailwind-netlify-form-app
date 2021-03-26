e62512d65b9632b07b34b08e214445992734076e
nrm176 <>
2021-03-26 16:12:02 +0900

    back to a tag

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="container">
       <p>
-        <NuxtLink to="/contact" no-prefetch>お問い合わせv2</NuxtLink>
+        <a href="/contact">お問い合わせv2</a>
       </p>
       <p>
         <a href="/contactv2">お問い合わせv1</a>
